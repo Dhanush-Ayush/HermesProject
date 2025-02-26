6f78220ea41ff93c2b361ac0ebdaa881d8a737b5
Dhanush-Ayush <bdhanush@google.com>
2025-02-27 04:30:09 +0530

    Create contact.js

---
+++ Javascript/contact.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".lead-form");
+    const thankYouMessage = document.querySelector(".thank-you-message");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent default form submission behavior
+        
+        // Hide form and show thank-you message
+        form.style.display = "none";
+        thankYouMessage.style.display = "block";
+    });
+});
+
